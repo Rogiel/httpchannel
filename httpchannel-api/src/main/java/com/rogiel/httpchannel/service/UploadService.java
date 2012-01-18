f8d5303f70c5d8893baef93127f9cf32febe070f
Rogiel <rogiel@rogiel.com>
2012-01-18 00:53:18 -0200

    Switches the license to Apache Software License, Version 2.0

--- httpchannel-api/src/main/java/com/rogiel/httpchannel/service/UploadService.java
+++ httpchannel-api/src/main/java/com/rogiel/httpchannel/service/UploadService.java
@@ -1,18 +1,20 @@
 /*
- * This file is part of seedbox <github.com/seedbox>.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * seedbox is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * seedbox is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with seedbox.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package com.rogiel.httpchannel.service;
 
@@ -40,7 +42,7 @@ public interface UploadService<C extends UploaderConfiguration> extends Service 
 	 * @return the new {@link Uploader} instance
 	 */
 	Uploader<C> getUploader(String filename, long filesize, C configuration);
-	
+
 	/**
 	 * Creates a new instance of {@link Uploader}. This instance is attached
 	 * with the parent {@link Service} instance.<br>
@@ -53,14 +55,13 @@ public interface UploadService<C extends UploaderConfiguration> extends Service 
 	 * @return the new {@link Uploader} instance
 	 */
 	Uploader<C> getUploader(String filename, long filesize);
-	
+
 	/**
 	 * Creates a new configuration object. If a service does not support or
 	 * require configuration, {@link NullUploaderConfiguration} should be
 	 * returned.
 	 * 
-	 * @return a new configuration object or
-	 *         {@link NullUploaderConfiguration}
+	 * @return a new configuration object or {@link NullUploaderConfiguration}
 	 */
 	C newUploaderConfiguration();
 
