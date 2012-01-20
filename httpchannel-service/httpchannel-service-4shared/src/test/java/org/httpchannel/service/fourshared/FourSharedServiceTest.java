6f608029b316b2c70809fef3df85357a6ee4dfea
Rogiel <rogiel@rogiel.com>
2012-01-20 00:52:51 -0200

    Removes uneeded httpchannel-service-4shared dependency

--- httpchannel-service/httpchannel-service-4shared/src/test/java/org/httpchannel/service/fourshared/FourSharedServiceTest.java
+++ httpchannel-service/httpchannel-service-4shared/src/test/java/org/httpchannel/service/fourshared/FourSharedServiceTest.java
@@ -5,11 +5,14 @@ package org.httpchannel.service.fourshared;
 
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.rogiel.httpchannel.service.helper.AuthenticationServices;
@@ -21,11 +24,20 @@ import com.rogiel.httpchannel.util.ChannelUtils;
  */
 public class FourSharedServiceTest {
 	private final FourSharedService service = new FourSharedService();
+	private final Properties properties = new Properties();
+
+	@Before
+	public void setUp() throws IOException {
+		properties.load(Files.newInputStream(Paths
+				.get("../src/test/resources/login.properties")));
+	}
 
 	@Test
 	public void test() throws IOException {
-		AuthenticationServices.authenticator(service, "", "").login();
-		
+		AuthenticationServices.authenticator(service,
+				properties.getProperty("4shared.username"),
+				properties.getProperty("4shared.password")).login();
+
 		final Path path = Paths
 				.get("../src/test/resources/upload-test-file.txt");
 		final URI uri = ChannelUtils.upload(service, path);
